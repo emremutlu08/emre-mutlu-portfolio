07939ca0f90b83524b79ffaf961a8eef1b35817e
emremutlu08 <mutluemre93@gmail.com>
2021-05-16 17:22:35 +0300

    tailwindcss/jit settings

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
-import Head from 'next/head'
+import Head from "next/head";
+import tw from "tailwind-styled-components";
+
+const Container = tw.p`
+    flex
+    items-center
+    justify-center
+    flex-col
+    w-full
+    bg-green-500
+`;
 
 export default function Home() {
   return (
@@ -10,20 +20,20 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+        <Container className="mt-3 text-2xl">
+          Get started by editing{" "}
+          <code className="p-3 font-mono text-lg  rounded-md">
             pages/index.js
           </code>
-        </p>
+        </Container>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full bg-indigo-100">
           <a
             href="https://nextjs.org/docs"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
@@ -73,10 +83,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
